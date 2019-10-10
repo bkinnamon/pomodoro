92e30ae708d9253bddd3848ed113e9240c576171
Brett Kinnamon <b.kinnamon@icloud.com>
2019-10-10 08:06:09 -0400

    blink when finished

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ const twoDigitString = num => num >= 10 ? num : `0${num}`;
 export default () => {
   const [elapsed, setElapsed] = React.useState(0);
   const [maxTime, setMaxTime] = React.useState(0);
+  const [darkMode, setDarkMode] = React.useState(false);
 
   const minutes = Math.floor((maxTime - elapsed) / secondsInMin)
   const seconds = (maxTime - elapsed) % secondsInMin
@@ -33,20 +34,30 @@ export default () => {
     interval = setInterval(() => setElapsed(e => e + 1), millisInSec);
   }
 
+  const toggleDarkMode = () => {
+    setDarkMode(darkMode => !darkMode)
+  }
+
   if (elapsed === maxTime && interval) {
     stopTimer();
+    toggleDarkMode()
+    setTimeout(toggleDarkMode, 500)
+    setTimeout(toggleDarkMode, 1000)
+    setTimeout(toggleDarkMode, 1500)
+    setTimeout(toggleDarkMode, 2000)
+    setTimeout(toggleDarkMode, 2500)
   }
 
   return (
     <Layout>
       <div>
-        <TimeDisplay>{time}</TimeDisplay>
+        <TimeDisplay isDark={darkMode}>{time}</TimeDisplay>
       </div>
       <div>
         <Button onClick={() => startTimer(25)} label="start work">
           <FontAwesomeIcon icon={faBriefcase} />
         </Button>
-        <Button onClick={() => startTimer(5)} label="start break">
+        <Button onClick={() => startTimer(0.1)} label="start break">
           <FontAwesomeIcon icon={faCoffee} />
         </Button>
         <Button onClick={() => stopTimer()} label="stop">
